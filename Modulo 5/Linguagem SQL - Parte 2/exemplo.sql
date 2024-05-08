c268259adde8e79d41c8c3cd88d6ebe94b6bbb15
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-05-08 13:48:50 -0300

    aula 37: funções de agregação

--- Modulo 5/Linguagem SQL - Parte 2/exemplo.sql
+++ Modulo 5/Linguagem SQL - Parte 2/exemplo.sql
@@ -28,6 +28,22 @@ CREATE TABLE consumidor(
   pais VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE categoria(
+	idCategoria INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(60) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE produto(
+	idProduto INT NOT NULL AUTO_INCREMENT, 
+	descricao VARCHAR(60) NOT NULL,
+	preco_venda DECIMAL(8,2) NOT NULL,
+	preco_custo DECIMAL(8,2) NOT NULL,
+	idCategoria int NOT NULL,
+	PRIMARY KEY (idProduto),
+	FOREIGN KEY (idCategoria) REFERENCES categoria(idCategoria)
+);
+
 INSERT INTO profissao (cargo) VALUES ('Programador');
 INSERT INTO profissao (cargo) VALUES ('Analista de Sistemas');
 INSERT INTO profissao (cargo) VALUES ('Suporte');
@@ -41,3 +57,8 @@ INSERT INTO cliente (nome, data_nasc, telefone, idProfissao) VALUES ('Carlos da 
 INSERT INTO consumidor (nome, telefone, endereco, cidade, cep, pais) VALUES ('Jose Carlos', '+5514912245678', 'Rua das Ruas', 'Cidadão', '85222-000', 'Brasil');
 INSERT INTO consumidor (nome, telefone, endereco, cidade, cep, pais) VALUES ('Marcos Vinicius', '+5514915245678', 'Rua de Novo', 'Cityzinha', '78632-000', 'Brasil');
 INSERT INTO consumidor (nome, telefone, endereco, cidade, cep, pais) VALUES ('Pedro Luiz', '+5514919645678', 'Av. das Avenidas', 'Cityzona', '97611-123', 'Brasil');
+
+INSERT INTO produto (descricao, preco_venda, preco_custo, idCategoria) VALUES ('Caderno', '5.45', '2.30', 1);
+INSERT INTO produto (descricao, preco_venda, preco_custo, idCategoria) VALUES ('Caneta', '1.20', '0.45', 1);
+INSERT INTO produto (descricao, preco_venda, preco_custo, idCategoria) VALUES ('Pendrive 32GB', '120.54', '32.55', 2);
+INSERT INTO produto (descricao, preco_venda, preco_custo, idCategoria) VALUES ('Mouse', '17.00', '4.30', 2);
